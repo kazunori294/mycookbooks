57a648698b8b8344cb9cb546a61e6e69693a9c05
root <root@lab-kickstart01.kazutan.info>
2014-07-26 00:54:01 +0900

    test

--- scripts/autoreg.sh
+++ scripts/autoreg.sh
@@ -1,24 +1,3 @@
 #!/bin/sh
 
-IPADDRESS=`/sbin/ifconfig eth0 | grep 'inet addr:' | sed -e 's/^.*inet addr://' -e 's/ .*//'`
-CURHOST=`hostname`
-AFHOST=`curl -# http://lab-infra01.kazutan.info/get_hostname?ipaddress=${IPADDRESS}`
-
-
-curl -# "lab-infra01.kazutan.info/reg_api?hostname=${CURHOST}&ipaddress=${IPADDRESS}"
-
-
-if test "${CURHOST}" = "${AFHOST}"; then
-   echo 'match'
-   echo 'no action needed'
-
-else
-   echo 'not match'
-   hostname ${AFHOST}
-
-   cat << EOF > /etc/sysconfig/network
-NETWORKING=yes
-HOSTNAME=${AFHOST}
-EOF
-
-fi
+curl -# http://lab-kickstart01.kazutan.info/scripts/autoreg2.sh | env bash
