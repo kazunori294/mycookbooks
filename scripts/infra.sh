55dc006801ce4e244c8a2caacd98e799bc283170
root <root@lab-kickstart01.kazutan.info>
2014-09-13 08:33:48 +0900

    bug fix

--- scripts/infra.sh
+++ scripts/infra.sh
@@ -2,7 +2,6 @@
 
 IPADDRESS=`/sbin/ifconfig eth0 | grep 'inet addr:' | sed -e 's/^.*inet addr://' -e 's/ .*//'`
 CURHOST=`hostname`
-AFHOST=`curl -# http://lab-infra01.kazutan.info/get_hostname?ipaddress=${IPADDRESS}`
 SUBNET=`/sbin/ifconfig eth0 | grep 'inet addr:' | sed -e 's/^.*Mask://' -e 's/ .*//'`
 MACADDRESS=`/sbin/ifconfig eth0 | grep 'HWaddr' | sed -e 's/^.*HWaddr //' -e 's/ .*//'`
 OS=`cat /etc/redhat-release | sed "s/ /+/g"`
@@ -14,6 +13,8 @@ MEM=`cat /proc/meminfo | grep MemTotal | awk '{print $2,$3}' | sed "s/ /+/g"`
 
 curl -# "lab-infra01.kazutan.info/reg_api?hostname=${CURHOST}&ipaddress=${IPADDRESS}&subnet=${SUBNET}&macaddress=${MACADDRESS}&os=${OS}&kernel=${KERNEL}&uptime=${UPTIME}&cpufm=${CPUFM}&cpunum=${CPUNUM}&mem=${MEM}"
 
+
+AFHOST=`curl -# http://lab-infra01.kazutan.info/get_hostname?ipaddress=${IPADDRESS}`
 
 if test "${CURHOST}" = "${AFHOST}"; then
    echo 'match'
