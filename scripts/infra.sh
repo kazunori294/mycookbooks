026c8b7bed5b5a2083d11f6c58cda7519ae86955
root <root@lab-kickstart01.kazutan.info>
2014-09-13 09:24:20 +0900

    bug fix

--- scripts/infra.sh
+++ scripts/infra.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
 
-IPADDRESS=`/sbin/ifconfig eth0 | grep 'inet addr:' | sed -e 's/^.*inet addr://' -e 's/ .*//'`
+ETH=`/sbin/ifconfig eth0 | grep 'inet addr:' | sed -e 's/^.*inet addr://' -e 's/ .*//' | wc -l`
+
+if test "${ETH}" = "1"; then
+   IPADDRESS=`/sbin/ifconfig eth0 | grep 'inet addr:' | sed -e 's/^.*inet addr://' -e 's/ .*//'`
+   SUBNET=`/sbin/ifconfig eth0 | grep 'inet addr:' | sed -e 's/^.*Mask://' -e 's/ .*//'`
+   MACADDRESS=`/sbin/ifconfig eth0 | grep 'HWaddr' | sed -e 's/^.*HWaddr //' -e 's/ .*//'`
+
+else
+   IPADDRESS=`/sbin/ifconfig br0 | grep 'inet addr:' | sed -e 's/^.*inet addr://' -e 's/ .*//'`
+   SUBNET=`/sbin/ifconfig br0 | grep 'inet addr:' | sed -e 's/^.*Mask://' -e 's/ .*//'`
+   MACADDRESS=`/sbin/ifconfig br0 | grep 'HWaddr' | sed -e 's/^.*HWaddr //' -e 's/ .*//'`
+
+fi
+
 CURHOST=`hostname`
-SUBNET=`/sbin/ifconfig eth0 | grep 'inet addr:' | sed -e 's/^.*Mask://' -e 's/ .*//'`
-MACADDRESS=`/sbin/ifconfig eth0 | grep 'HWaddr' | sed -e 's/^.*HWaddr //' -e 's/ .*//'`
 OS=`cat /etc/redhat-release | sed "s/ /+/g"`
 KERNEL=`uname -r | sed "s/ /+/g"`
 UPTIME=`uptime | awk '{print $3,$4,$5}' | sed "s/,//g" | sed "s/ /+/g"`
-CPUFM=`cat /proc/cpuinfo | grep "model name" | awk -F":" '{print $2}' | sed "s/ /+/g"`
+CPUFM=`cat /proc/cpuinfo | grep "model name" | awk -F":" '{print $2}' | head -n 1 |sed "s/ /+/g"`
 CPUNUM=`cat /proc/cpuinfo | grep processor | wc -l`
 MEM=`cat /proc/meminfo | grep MemTotal | awk '{print $2,$3}' | sed "s/ /+/g"`
 
