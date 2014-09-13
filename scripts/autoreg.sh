58d69c769c1c63948df9b4b05fc594c696d30078
root <root@git-test.kazutan.info>
2014-09-14 07:55:20 +0900

    test

--- scripts/autoreg.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-curl -# http://lab-kickstart01.kazutan.info/scripts/infra.sh | env bash
--- scripts/sethost.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cat << EOF > /etc/sysconfig/network
-NETWORKING=yes
-HOSTNAME=$1
-EOF
